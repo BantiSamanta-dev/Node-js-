494b0c651948b3f692eebecc1d47bd83b7096f1a
Banti samanta <123482198+fathi0x@users.noreply.github.com>
2024-03-03 15:57:15 +0530

    just comment in code to understand the code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-function loginmiddelware(req ,res ,next){
+function loginmiddelware(req ,res ,next){   /// this is the middelware  
     console.log('login cheak')
-    next()
+    next()   /// the is the fuction that tell the code to run the next function 
 }
 
 
@@ -12,6 +12,6 @@ console.log("login scuessfull!!")
 res.send('<h1>home page</h1>')
 }
 
-app.get('/', loginmiddelware ,homepage)
+app.get('/', loginmiddelware ,homepage)  // there we run the middelware  after the homepage fuction  
 
 app.listen(3000)
